495f93292f9d8a275e6ee4d5e4cd836d251e082e
honghyunAhn <anh2073@naver.com>
2021-03-21 21:27:12 +0900

    Study JS

---
+++ .history/momentum_20210321204400.js
@@ -0,0 +1,6 @@
+function sayHello(name){
+    console.log('Hello!', name);
+}
+
+sayHello("anh288");
+console.log("Hi");
---
+++ .history/momentum_20210321211334.js
@@ -0,0 +1,14 @@
+// function sayHello(name, age){
+//     console.log('Hello!', name, "you have", age, "years of age.");
+// }
+
+// sayHello("anh288", 15);
+// console.log("Hi");
+
+function sayHello(name, age){
+    return console.log(`Hello ${name} you are ${age} years old`)
+}
+
+sayHello("anh288", 15);
+
+const greatAnh288 = sayHello("anh288", 15);
