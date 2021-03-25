0b5d7fe79dea5a787931bdf3acb0422a7b9f65aa
honghyunAhn <anh2073@naver.com>
2021-03-25 17:19:55 +0900

    JS:Events and event handlers

--- momentum.js
+++ momentum.js
@@ -1,6 +1,16 @@
 // const title = document.getElementById("title");
 const title = document.querySelector("#title");
-title.innerHTML = "Hi! From JS"; 
-title.style.color = "red"
+// title.innerHTML = "Hi! From JS"; 
+// title.style.color = "red"
 // console.dir(document);
-document.title = "I own you now";
+// document.title = "I own you now";
+function handleResize(event){
+    console.log(event);
+}
+
+function handleClick(){
+    title.style.color =  "red";
+}
+// window.addEventListener("resize", handleResize);
+
+title.addEventListener("click", handleClick);
