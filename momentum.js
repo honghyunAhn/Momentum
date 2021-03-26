b4facb392d59957fdcf2b3f07fdffad708b5e8e0
honghyunAhn <anh2073@naver.com>
2021-03-26 19:28:45 +0900

    If, else, and, or

--- momentum.js
+++ momentum.js
@@ -1,16 +1,14 @@
-// const title = document.getElementById("title");
-const title = document.querySelector("#title");
-// title.innerHTML = "Hi! From JS"; 
-// title.style.color = "red"
-// console.dir(document);
-// document.title = "I own you now";
-function handleResize(event){
-    console.log(event);
-}
+// if(10 === 5) {
+//     console.log("hi");
+// } else if ("10" === "10") {
+// } else {
+//     console.log("ho");
+// }
 
-function handleClick(){
-    title.style.color =  "red";
-}
-// window.addEventListener("resize", handleResize);
+const age = prompt("How old are you");
 
-title.addEventListener("click", handleClick);
+if(age > 18) {
+    console.log("you can drink");
+} else {
+    console.log("you can't drink");
+}
