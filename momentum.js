1ff661aacab989c211d7d10a36d07422d7035c04
honghyunAhn <anh2073@naver.com>
2021-03-28 23:30:24 +0900

    Function practice Two 1

--- momentum.js
+++ momentum.js
@@ -1,37 +1,18 @@
-// if(10 === 5) {
-//     console.log("hi");
-// } else if ("10" === "10") {
-// } else {
-//     console.log("ho");
-// }
+const title = document.querySelector("#title");
 
-const title = document.querySelector("title");
-
-const BASE_COLOR = "#34495e"
-const OTHER_COLOR = "#7f8c8d"
-
+const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentColor = title.style.color;
-    if(currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
     } else {
-        title.style.color = BASE_COLOR;
+        title.className = "";
     }
-}
-function init() {
-    title.style.color = BASE_COLOR;
+};
+
+function init(){
     title.addEventListener("click", handleClick);
-}
+};
+
 init();
-
-function handleOffline() {
-    console.log("Bye bye");
-}
-
-function handleOnline() {
-    console.log("Welcome back");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
