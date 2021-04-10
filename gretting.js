1faf070bff824032951d779351580adbbf47bef8
honghyunAhn <anh2073@naver.com>
2021-04-10 22:28:26 +0900

    Saving the User Name 3

--- gretting.js
+++ gretting.js
@@ -1,14 +1,22 @@
 const form =  document.querySelector(".js-form"),
-    input = form.querySelector("input");
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greeting");
 
-const USER_LS = "currentUser"
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+};
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
 
     } else {
-        
+        paintGreeting(currentUser);
     }
 }
 function init(){
